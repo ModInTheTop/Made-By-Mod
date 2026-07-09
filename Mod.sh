4740e00bcb9719e0fa63d77fc14cf75162c0a63a
ModInTheTop <baachirii51@gmail.com>
2026-07-09 11:01:19 +0200

    Update Mod.sh with new formatting and content

--- Mod.sh
+++ Mod.sh
@@ -11,7 +11,7 @@ echo -e '\033[31;40;1m
 ⠀⠀⠀⠀⠀⠀⠰⣿⣧⡈⠮⠜⠀⠀⢀⣼⣿⣏⢒⣢⣿⡝⠶⠯⠝⢃⡜⠀⠀        MultiTools Para Hacking                            
 ⠀⠀⡀⠀⠀⡀⠴⠝⡈⢻⣶⣄⣀⣴⡿⠊⢿⣽⣷⣭⣷⣬⡷⢖⡒⠋⠁⠀⠀      --------------------------
 ⠀⠀⠉⠒⡒⠂⢀⠠⠂⢠⠿⣸⡷⢾⡇⠀⢸⢡⣉⣒⡊⠉⠲⣀⢪⣑⢤⡀⠀      
-⠚⠯⡵⣮⡄⡀⡁⠔⣺⠈⠏⠀⠁⠫⡂⠀⠀⠑⢄⠀⠀⢢⢠⠿⡌⣿⣧⡇⠀    ⁺˚⋆｡° 𝐌𝐚𝐝𝐞 𝐁𝐲: 𝐁𝐚𝐚𝐜𝐡𝐢𝐫𝐢𝐢 °｡⋆˚⁺
+⠚⠯⡵⣮⡄⡀⡁⠔⣺⠈⠏⠀⠁⠫⡂⠀⠀⠑⢄⠀⠀⢢⢠⠿⡌⣿⣧⡇⠀         𝐌𝐚𝐝𝐞 𝐁𝐲: 𝐁𝐚𝐚𝐜𝐡𝐢𝐫𝐢𝐢
 ⠀⠀⠀⠉⠀⠀⢀⠴⡑⢇⠈⡄⠀⠀⠇⠀⠂⠀⠀⠑⢦⡟⠀⠊⣀⢯⡿⠁⠀
 ⠀⠀⠀⠐⠶⢬⣶⡯⠈⡸⠀⠘⠄⠀⣿⠀⠀⠢⣄⠀⠀⠑⣦⡁⠁⢏⠀⠀⠀ 
 ⠀⠀⠀⠘⢤⣄⠀⠄⠊⢠⡇⠀⠈⢄⠸⡖⣆⣤⣈⠳⣠⡺⠉⢹⡄⣿⠀⠀⠀
@@ -30,9 +30,9 @@ echo -e '\033[31;40;1m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠡⠁⠀⠀⠀⠀⠀
 \033[33;4mVersion:\033[0m 1.0⠀                      \033[33;4mCTRL+C:\033[0m Exit
 
-\e[37m[1]\e[36m Requisitos y actualización       \e[37m[2]\e[36m Phishing Tool				
+\e[37m[1]\e[36m Requisitos y actualización     \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m Subscan			
-\e[37m[5]\e[36m Gmail Bomber		      \e[37m[6]\e[36m DDOS Attack			
+\e[37m[5]\e[36m Gmail Bomber		    \e[37m[6]\e[36m DDOS Attack			
 \e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
 \e[37m[11]\e[36m HackerPro                    \e[37m[12]\e[36m RED_HAWK
 \e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
